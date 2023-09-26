fa852ab00aa266b188c1b1c12ee93170fe75279e
kotiki <kadochnikovandrew@gmail.com>
2023-09-27 00:13:36 +0300

    sdsd

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,24 +1,28 @@
-import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
+link = " http://suninjuly.github.io/find_xpath_form"
 
-class TestAbs(unittest.TestCase):
-    def test_abs1(self):
-        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
 
-    def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+    input1 = browser.find_element(By.TAG_NAME, 'input')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, 'last_name')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, 'city')
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.XPATH, "//button[contains(text(),'Submit')]")
+    button.click()
 
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
 
-if __name__ == "__main__":
-    unittest.main()
-
-    if __name__ == "__main__":
-        unittest.main()
-
-
-
-
-
-
-
-
+# не забываем оставить пустую строку в конце файла
